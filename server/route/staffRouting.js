b20af9cdfc8fbb8bb82231d082e59b773c5ef564
NoyCoca <76784742+NoyCoca@users.noreply.github.com>
2021-09-14 15:53:47 +0300

    Update staffRouting.js

--- server/route/staffRouting.js
+++ server/route/staffRouting.js
@@ -9,21 +9,3 @@ router.delete('/',authRole("Staff"), staffController.deleteStaffById);
 router.put('/:id',authRole("Staff"), staffController.updateStaffById);
 
 module.exports = router;
-
-// router.get('/student/',authRole("Staff"),staffController.getStudentsByStaff)
-// const getStudentsByStaff = async (req, res) => {
-//     try {
-//         await StaffModel.findById(req.body.id)
-//             .populate('students')
-//             .then(staff => {
-//                 res.status(201).json({ massage: 'The student is ', data: staff.students.map((student) => student ) })
-//             })
-//             .catch(err => {
-//                 res.status(500).json({ massage: 'error with population', data: err });
-//             })
-  
-//     }
-//     catch (err) {
-//         res.status(500).json({ massage: "wrong", error: err })
-//     }
-//   }
