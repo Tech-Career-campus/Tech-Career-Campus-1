901df84baaa4ad696b7ecfd67e676207cff64c42
SHUANESH <shuanesh1817@gmail.com>
2021-09-15 09:32:08 +0300

    uploge margim

--- server/route/loginRouting.js
+++ server/route/loginRouting.js
@@ -9,12 +9,7 @@ const {authRole} = require('../controller/authentication/auth');
 const upload = require('../middleware/upload');
 
 router.post('/login', login);
-<<<<<<< HEAD
-router.post('/register', isToken, authRole("Staff"), register);
-router.delete('/logout', logout);
-=======
 router.post('/register',isToken,authRole("Staff"),upload.single('profileImg'), register);
 router.get('/logout',logout);
->>>>>>> fa0b74587b822539d9e85286ab3369e147347ef4
 
 module.exports = router;
