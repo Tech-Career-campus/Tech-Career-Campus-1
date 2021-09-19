376b4208908bc6ca1cdacdfc53a4c6bf399e500f
SlumpG <shlomo.gangul1@gmail.com>
2021-09-19 14:40:34 +0300

    fixed the news

--- client/src/Components/Features/Navbar/NavbarComponent.jsx
+++ client/src/Components/Features/Navbar/NavbarComponent.jsx
@@ -4,14 +4,6 @@ import { Link } from "react-router-dom";
 import techLogo from "../../../images/tech-logo.jpeg";
 import "./Navbar.css";
 
-<<<<<<< HEAD
-  const logout = () => {
-    localStorage.removeItem("jwtToken");
-    window.location.href = "./";
-  };
-
-export default function Navbar() {
-=======
 const logout = () => {
   localStorage.removeItem("jwtToken");
   window.location.href = "./";
@@ -20,7 +12,6 @@ const logout = () => {
 const Navbar = () => {
   const { user } = useSelector((state) => state.user);
   console.log(user);
->>>>>>> a392d7be080a9d8626a91e4d9ffe0e43fd315ab0
   return (
     <>
       <div className="navbar-main">
