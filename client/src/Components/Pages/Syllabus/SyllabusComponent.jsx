02acacc6c32d01d2567cd9ccd7e19359e2b8906f
SlumpG <shlomo.gangul1@gmail.com>
2021-09-22 21:13:32 +0300

    duck

--- client/src/Components/Pages/Syllabus/SyllabusComponent.jsx
+++ client/src/Components/Pages/Syllabus/SyllabusComponent.jsx
@@ -5,6 +5,11 @@ import {
   updateSyllabus,
   updateSubSubject,
 } from "../../../Redux/actions/SyllabusAction";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import Timeline from "@material-ui/lab/Timeline";
 import TimelineItem from "@material-ui/lab/TimelineItem";
 import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
@@ -14,7 +19,9 @@ import TimelineDot from "@material-ui/lab/TimelineDot";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { hebrewVariables } from "../../../utils/hebrewVariables";
-
+import WorkIcon from "@material-ui/icons/Work";
+import SchoolIcon from "@material-ui/icons/School";
+import StarIcon from "@material-ui/icons/Star";
 
 const SyllabusComponent = () => {
   const syllabus = useSelector((state) => state.syllabus);
@@ -38,151 +45,158 @@ const SyllabusComponent = () => {
       ...subSubject,
       _id: courseId,
       courseInformationId: subjectId,
-      array, array,
+      array,
+      array,
       array_id: array_id,
       arrayField: e.target.name,
-      newValue: e.target.value
+      newValue: e.target.value,
     });
   };
 
   const dispatch = useDispatch();
   useEffect(() => dispatch(getSyllabus(course)), [dispatch, course]);
   useEffect(() => {
-    setcourseId(syllabus._id)
-  }, [HandleChange, HandleTopicChange])
+    setcourseId(syllabus._id);
+  }, [HandleChange, HandleTopicChange]);
   return (
-    <>
-      {
+   
         <>
           <h1>{syllabus.name}</h1>
 
-          <Timeline position="alternate">
-            {syllabus?.CourseInformation?.map((courseItem,index) => {
-
+          <VerticalTimeline>
+            {syllabus?.CourseInformation?.map((courseItem, index) => {
               return (
-                <div key={index}>
-                  <TimelineItem>
-                    <TimelineSeparator>
-                      <TimelineDot />
-                      <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                      <Paper elevation={6} style={{backgroundColor:'#f1f1f1'}} >
-                        <Typography variant="h6" component="h1">
-                          <h2>{courseItem.nameSubject}</h2>
+                  <VerticalTimelineElement
+                  key={index}
+                    className="vertical-timeline-element"
+                    contentStyle={{
+                      background: "transparent",
+                      color: "#fff",
+                      padding: "0"
+                    }}
+                    contentArrowStyle={{
+                      borderRight: "7px solid  rgb(0, 0, 0)",
+                    }}
+                    date="2011 - present"
+                    iconStyle={{
+                      background: "rgb(210, 235, 255)",
+                      color: "#fff",
+                    }}
+                   position={(index+1) % 2 === 0 ? "right" : "left"}
+                  >
+                    <Paper elevation={6} style={{ backgroundColor: "#f1f1f1" }}>
+                      <Typography variant="h6" component="h1">
+                        <h2>{courseItem.nameSubject}</h2>
 
-                          <button
-                            className="btn"
-                            onClick={(e) => {
-                              setIsClicked(true);
-                            }}
-                          >
-                            {hebrewVariables.edit}
-                          </button>
-                          {isClicked ? (
-                            <div>
-                              <input
-                                placeholder={courseItem.nameSubject}
-                                name="nameSubject"
-                                type="text"
-                                onChange={(e) =>
-                                  HandleChange(e, courseItem._id)
-                                }
-                              />
-                              <button
-                                type="button"
-                                onClick={(e) => {
-                                  setIsClicked(false);
-                                  dispatch(updateSyllabus(newSyllabus,e));
-                                }}
-                                className="btn"
-                              >
-                                {hebrewVariables.addChanges} 
-                              </button>
-                            </div>
-                          ) : null}
-                        </Typography>
-                        <Typography style={{padding:'20px'}} sx={{ m: 3 }}>
-                          <h4> 
-                            <ul style={{listStyleType:'none',}}>
-                              {courseItem.topics.map((topic, index) => {
-                                return (
-                                  <div>
-                                    <li key={topic._id}>{topic.subject} </li>
-                                    {isClicked ? (
-                                      <div>
-                                        <label htmlFor="" style={{ display: 'inline' }}>
-                                          {` ${++index}`}
-                                        </label>
-                                        <input
-              
-                                          key={topic._id}
-                                          name="subject"
-                                          type="text"
-                                          placeholder={topic.subject}
-                                          onChange={(e) =>
-                                            HandleTopicChange(e, courseItem._id, "topics", topic._id)
-                                          }
-                                          type="text"
-                                        />
-                                        <button
-                                          type="button"
-                                          value={topic._id}
-                                          onClick={() => {
-                                            setIsClicked(false);
-                                            dispatch(updateSubSubject(subSubject))
-                                          }
-                                          }
-
-                                          className="btn"
-                                        >
-                                           {hebrewVariables.addChanges} 
-                                          
-                                        </button>
-                                      </div>
-                                    ) : null}
-                                  </div>
-                                );
-                              })}
-                            </ul>
-                          </h4>
-                          <p>{courseItem.summery}</p>
-
-
-                          
-
-                          <ul style={{listStyleType:'none'}}>
-
-                            <h3>Links:</h3>
-                            {courseItem.links.map((link) => {
+                        <button
+                          className="btn"
+                          onClick={(e) => {
+                            setIsClicked(true);
+                          }}
+                        >
+                          {hebrewVariables.edit}
+                        </button>
+                        {isClicked ? (
+                          <div>
+                            <input
+                              placeholder={courseItem.nameSubject}
+                              name="nameSubject"
+                              type="text"
+                              onChange={(e) => HandleChange(e, courseItem._id)}
+                            />
+                            <button
+                              type="button"
+                              onClick={(e) => {
+                                setIsClicked(false);
+                                dispatch(updateSyllabus(newSyllabus, e));
+                              }}
+                              className="btn"
+                            >
+                              {hebrewVariables.addChanges}
+                            </button>
+                          </div>
+                        ) : null}
+                      </Typography>
+                      <Typography style={{ padding: "20px" }} sx={{ m: 3 }}>
+                        <h4>
+                          <ul style={{ listStyleType: "none" }}>
+                            {courseItem.topics.map((topic, index) => {
                               return (
-                                <>
-                                  <li>
-                                    {" "}
-                                    <a href={link.tasks}>Google Drive</a>{" "}
-                                  </li>
-                                  <li>
-                                    {" "}
-                                    <a href={link.Presentations}>
-                                      Presentation
-                                    </a>{" "}
-                                  </li>
-                                </>
+                                <div>
+                                  <li key={topic._id}>{topic.subject} </li>
+                                  {isClicked ? (
+                                    <div>
+                                      <label
+                                        htmlFor=""
+                                        style={{ display: "inline" }}
+                                      >
+                                        {` ${++index}`}
+                                      </label>
+                                      <input
+                                        key={topic._id}
+                                        name="subject"
+                                        type="text"
+                                        placeholder={topic.subject}
+                                        onChange={(e) =>
+                                          HandleTopicChange(
+                                            e,
+                                            courseItem._id,
+                                            "topics",
+                                            topic._id
+                                          )
+                                        }
+                                        type="text"
+                                      />
+                                      <button
+                                        type="button"
+                                        value={topic._id}
+                                        onClick={() => {
+                                          setIsClicked(false);
+                                          dispatch(
+                                            updateSubSubject(subSubject)
+                                          );
+                                        }}
+                                        className="btn"
+                                      >
+                                        {hebrewVariables.addChanges}
+                                      </button>
+                                    </div>
+                                  ) : null}
+                                </div>
                               );
                             })}
                           </ul>
-                        </Typography>
-                      </Paper>
-                    </TimelineContent>
-                  </TimelineItem>
-                </div>
+                        </h4>
+                        <p>{courseItem.summery}</p>
+
+                        <ul style={{ listStyleType: "none" }}>
+                          <h3>Links:</h3>
+                          {courseItem.links.map((link) => {
+                            return (
+                              <>
+                                <li>
+                                  {" "}
+                                  <a href={link.tasks}>Google Drive</a>{" "}
+                                </li>
+                                <li>
+                                  {" "}
+                                  <a href={link.Presentations}>
+                                    Presentation
+                                  </a>{" "}
+                                </li>
+                              </>
+                            );
+                          })}
+                        </ul>
+                      </Typography>
+                    </Paper>
+                  </VerticalTimelineElement>
               );
             })}
-          </Timeline>
+          </VerticalTimeline>
         </>
-      }
+     
       )
-    </>
-  );
+   
 };
 export default SyllabusComponent;
