2fc35a7cb8fb53e2b622a3c79866a13a3ac1674c
itzhackB <yzikbynesai@gmail.com>
2021-09-22 15:50:40 +0300

    staff updates

---
+++ client/src/Components/Features/StaffCard/StaffCard.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { deleteStaff, updateStaff } from '../../../Redux/actions/staffAction';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import staffImg from '../../../images/1632247546163.jpg'
+import handleChange from "../../../utils/handleChange";
+import { hebrewVariables } from '../../../utils/hebrewVariables';
+
+
+
+
+const StaffCard = ({ staffItem }) => {
+    const dispatch = useDispatch();
+
+    const [isEdit, setIsEdit] = useState(false)
+    const [staffUpate, setStaffUpdate] = useState({ ...staffItem });
+
+    const deletestaffHandler = (_id) => {
+        dispatch(deleteStaff(_id))
+    }
+
+    const { firstName, lastName, phone, email, jod, responsible, _id } = staffItem
+
+
+    return (
+        <div>
+
+            {
+                isEdit ?
+                    (<>
+                        <div >
+                            <img src={staffImg} width={"200px"} height={"200px"} alt={"staff"} />
+                        </div>
+                        <div >
+                            <h1>{firstName} {lastName}</h1>
+                        </div>
+
+                        <lable>{hebrewVariables.responsibleFor}</lable>
+                        <input
+                            name="responsible"
+                            label
+                            type="text"
+                            onChange={(e) => handleChange(e, staffUpate, setStaffUpdate)}
+                            value={staffUpate.responsible}
+                        />
+                        <lable>{hebrewVariables.job}</lable>
+                        <input
+                            name="jod"
+                            label
+                            type="text"
+                            onChange={(e) => handleChange(e, staffUpate, setStaffUpdate)}
+                            value={staffUpate.jod}
+                        />
+                        <div variant="body2" color="text.secondary">
+                            <h3>{hebrewVariables.contact}</h3>
+                            <ul>
+                                <li>
+                                    <input type="phone" value={staffUpate.phone}
+                                        onChange={(e) => handleChange(e, staffUpate, setStaffUpdate)}
+                                    />
+                                </li>
+                            </ul>
+                        </div>
+                        <Button onClick={() => {
+                            setIsEdit(!isEdit);
+                        }}>
+                            {hebrewVariables.closeBtn}
+                        </Button>
+
+                        <Button
+                            onClick={() => {
+                            dispatch(updateStaff(staffUpate));
+                            }}
+                        >
+                            {hebrewVariables.update}
+                        </Button>
+                    </>)
+                    :
+                    (<div>
+                        <div >
+                            <img src={staffImg} width={"200px"} height={"200px"} alt={"staff"} />
+                        </div>
+                        <div>
+                            <div >
+                                <h1>{firstName} {lastName}</h1>
+                            </div>
+                            <div>
+                                <h3>{jod}</h3>
+                            </div>
+                            <div variant="body2" color="text.secondary">
+                                <p>
+                                    <h4>{hebrewVariables.responsibleFor}</h4>
+                                    {responsible}
+                                </p>
+                            </div>
+                            <div variant="body2" color="text.secondary">
+                                <h3>{hebrewVariables.contact}</h3>
+                                <ul>
+                                    <li>{phone}</li>
+                                    <li>{email}</li>
+                                </ul>
+                            </div>
+
+                        </div>
+                        <Button>
+                            <DeleteIcon onClick={() => deletestaffHandler(staffItem._id)} />
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                setIsEdit(!isEdit);
+                                setStaffUpdate({ ...staffUpate, _id: staffItem._id });
+                            }}
+                        >
+                            {hebrewVariables.update}
+                        </Button>
+
+                    </div>)
+            }
+
+
+
+
+        </div >
+    )
+}
+
+export default StaffCard
--- client/src/Redux/reducers/staffRedcuer.js
+++ client/src/Redux/reducers/staffRedcuer.js
@@ -1,23 +1,26 @@
-import { GET_STAFF_LIST, ADD_STAFF, DELETE_STAFF,STAFF_ERRORS, UPDATE_STAFF } from '../actions/types'
+import { GET_STAFF_LIST, ADD_STAFF, DELETE_STAFF, STAFF_ERRORS, UPDATE_STAFF } from '../actions/types'
 
 const staffRedcuer = (state = { staff: [], errors: {} }, action) => {
     switch (action.type) {
         case GET_STAFF_LIST:
+            debugger
             return {
                 ...state, staff: action.payload
             }
         case ADD_STAFF:
             return {
-                errors:{}, staff: [...state.staff ,action.payload]
+                errors: {}, staff: [...state.staff, action.payload]
             }
         case DELETE_STAFF:
-            return {...state, staff: state.staff.filter(item => item._id !== action.payload._id)}
-        case UPDATE_STAFF: 
+            return { ...state, staff: state.staff.filter(item => item._id !== action.payload._id) }
+
+        case UPDATE_STAFF:
+            debugger
             return {
-                ...state, staff: state.staff.map(staff => staff._id === action.payload._id ? action.payload : staff)
+                erros: {}, staff: state.staff.map(item => item._id === action.payload._id ? action.payload : item)
             }
         case STAFF_ERRORS:
-            return {...state, errors:action.payload}
+            return { ...state, errors: action.payload }
         default:
             return state;
     }
