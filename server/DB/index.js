439abe806841adfcff489ecc2b0d4b090b249211
NoyCoca <76784742+NoyCoca@users.noreply.github.com>
2021-09-29 03:47:46 +0300

    Update index.js

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,11 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const dbConnection = process.env.dbConnection;
-<<<<<<< HEAD
-const dbConnection_test = process.env.dbConnection_test
-=======
 const dbConnection_test = process.env.dbConnection_test;
->>>>>>> 6c63f616ee4eb4ab27e84f0b9e14b6ac4d262e21
 const mongoose = require("mongoose");
 const chalk = require("chalk");
 const options = {
@@ -15,7 +11,6 @@ const options = {
   useCreateIndex: true,
 };
 
-<<<<<<< HEAD
   if (process.env.TEST_ENV === "test") {
     console.log(dbConnection_test);
      mongoose
@@ -38,32 +33,6 @@ const options = {
         console.log(chalk.red("Connection error", err.message));
       });
     }
-=======
-if (process.env.TEST_ENV === "test") {
-console.log(dbConnection_test);
- mongoose
-  .connect(dbConnection_test, options)
-  .then(() => {
-    console.log(chalk.green("mongoDB connected"));
-  })
-  .catch((err) => {
-    console.log(chalk.red("Connection error", err.message));
-  });
-
-}else{
-  console.log(dbConnection);
-  mongoose
-  .connect(dbConnection, options)
-  .then(() => {
-    console.log(chalk.green("mongoDB connected"));
-  })
-  .catch((err) => {
-    console.log(chalk.red("Connection error", err.message));
-  });
-}
-
-
->>>>>>> 6c63f616ee4eb4ab27e84f0b9e14b6ac4d262e21
 
 const db = mongoose.connection;
 module.exports = db;
