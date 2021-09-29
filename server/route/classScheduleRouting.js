3677331a65ceb2556c437a6fe4d65a2f2a6ad518
NoyCoca <76784742+NoyCoca@users.noreply.github.com>
2021-09-29 03:53:31 +0300

    Update classScheduleRouting.js

--- server/route/classScheduleRouting.js
+++ server/route/classScheduleRouting.js
@@ -7,7 +7,7 @@ const { authRole } = require("../controller/authentication/auth");
 router.get("/", scheduleCtrl.getAllClasses);
 router.put("/", authRole(ADMIN), scheduleCtrl.updateClasses);
 router.post("/", authRole(ADMIN), scheduleCtrl.postClasses);
-router.delete("/",  scheduleCtrl.deleteClasses);
+router.delete("/", authRole(ADMIN), scheduleCtrl.deleteClasses);
 router.put("/updateClassesName",authRole(ADMIN), scheduleCtrl.updateClassesName);
 
 module.exports = router;
