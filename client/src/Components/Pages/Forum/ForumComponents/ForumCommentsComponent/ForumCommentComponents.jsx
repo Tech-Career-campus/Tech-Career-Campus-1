598873a3ae58eb4fa10963b1d21f0775294cbabe
SlumpG <shlomo.gangul1@gmail.com>
2021-09-30 12:05:33 +0300

    ?

--- client/src/Components/Pages/Forum/ForumComponents/ForumCommentsComponent/ForumCommentComponents.jsx
+++ client/src/Components/Pages/Forum/ForumComponents/ForumCommentsComponent/ForumCommentComponents.jsx
@@ -27,10 +27,9 @@ const ForumPostComponents = ({post}) => {
        {c.split(":")[1]}
         </li>
       ))}
-      <li ref={commentRef}></li>
+      <li style={{height:'0' ,backgroundColor:'transparent',overflow:'hidden'}} ref={commentRef}/>
       </ul>
       <div className="chat-window">
-       
         <input
           className="chat-window-message"
           name="chat-window-message"
