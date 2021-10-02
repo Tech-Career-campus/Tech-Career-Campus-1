06bd577efbd38f431d1c311c01da2dcbd12de937
CHEN ZAUDA <czauda@gmail.com>
2021-10-02 21:13:41 +0300

    search student

--- client/src/Redux/actions/studentsActions.js
+++ client/src/Redux/actions/studentsActions.js
@@ -2,7 +2,6 @@ import fetcher from '../../utils/fetcher';
 import { CREATE_STUDENT, CREATE_STUDENT_ERRORS, DELETE_STUDENT, GET_STUDENTS, UPDATE_STUDENT } from './types'
 export const getStudents = (courseId) => async dispatch => {
     await fetcher(`http://localhost:8080/api/course/students/${courseId}`)
-    .then(res=>console.log(res))
         .then((response) => dispatch({
             type: GET_STUDENTS,
             payload: response.data,
