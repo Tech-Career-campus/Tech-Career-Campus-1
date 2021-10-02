2fa09ef659434b041e6a05b24d18dbcfe4816055
itzhak12380 <iascc12380@gmail.com>
2021-10-03 01:56:34 +0300

    add change passwrod function

--- client/src/Components/Features/EditProfile/EditProfileConponent.jsx
+++ client/src/Components/Features/EditProfile/EditProfileConponent.jsx
@@ -11,21 +11,25 @@ import {
   updateStaffPassword,
 } from "../../../Redux/actions/staffAction";
 
-const EditProfile = ({ setOpen,open, user, setEditProfile}) => {
+const EditProfile = ({ setOpen, open, user, setEditProfile }) => {
   const [changePassword, setChangePassword] = useState(false);
+  const [currntPassword, setcurrntPassword] = useState("");
   //   const [updateUser, setUpdateUser] = useState({...user, password:"", newPassword:"", confirmPassword:"" })
   const [userUpdate, setUserUpdate] = useState({ ...user });
   const [file, setFile] = useState(null)
   const dispatch = useDispatch();
 
-  useEffect(() => setUserUpdate({ ...user}), [user]);
+  useEffect(() => setUserUpdate({ ...user }), [user]);
 
   const handleClose = () => setOpen(false);
-
+  // const handleClose = () => setOpen(false);
+  const currntpassword = (e) => {
+    setcurrntPassword(e.target.value)
+  }
   return (
-    
+
     <div className="edit-profile">
-        <Modal
+      <Modal
         keepMounted
         open={open}
         onClose={handleClose}
@@ -33,71 +37,91 @@ const EditProfile = ({ setOpen,open, user, setEditProfile}) => {
         aria-describedby="keep-mounted-modal-description"
       >
         <Box className="edit-profile-box">
-      <form className='edit-profile-form' onSubmit={(e) => e.preventDefault()}>
-        <label> {hebrewVariables.firstName}</label>
-        <input
-          value={userUpdate.firstName}
-          type="text"
-          name={"firstName"}
-          onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
-        />
-        <label>{hebrewVariables.lastName}</label>
-        <input
-          value={userUpdate.lastName}
-          type="text"
-          name={"lastName"}
-          onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
-        />
-        <label>{hebrewVariables.email} </label>
-        <input
-          value={userUpdate.email}
-          type="email"
-          name={"email"}
-          onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
-        />
-        <label>{hebrewVariables.phone} </label>
-        <input
-          value={userUpdate.phone}
-          type="tel"
-          name={"phone"}
-          onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
-        />
+          <form className='edit-profile-form' onSubmit={(e) => e.preventDefault()}>
+            <label> {hebrewVariables.firstName}</label>
+            <input
+              value={userUpdate.firstName}
+              type="text"
+              name={"firstName"}
+              onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
+            />
+            <label>{hebrewVariables.lastName}</label>
+            <input
+              value={userUpdate.lastName}
+              type="text"
+              name={"lastName"}
+              onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
+            />
+            <label>{hebrewVariables.email} </label>
+            <input
+              value={userUpdate.email}
+              type="email"
+              name={"email"}
+              onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
+            />
+            <label>{hebrewVariables.phone} </label>
+            <input
+              value={userUpdate.phone}
+              type="tel"
+              name={"phone"}
+              onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}
+            />
             <input accept="image/*" name="profileImg" type="file"
               onChange={e => {
-                const img=e.target.files[0]
+                const img = e.target.files[0]
                 setFile(img)
               }} />
             <div className={`edit-profile-btn-container ${changePassword ? "edit-profile-form" : ""}`}>
+              {changePassword === true ? (
+                <>
+                  <label>{hebrewVariables.currentPassword}</label>
+                  {/* של יצחק ב */}
+                  <input name="currentPassword" type="text" onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}/>
 
-        {changePassword ? (
-          <>
-            <label>{hebrewVariables.currentPassword}</label>
-            <input name="password" type="text" onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}/>
-            <label>{hebrewVariables.newPassword}</label>
-            <input name="newPassword" value={userUpdate.newPassword} type="text" onChange={(e) => handleChange(e, userUpdate, setUserUpdate)} />
-            <label>{hebrewVariables.confirmPassword}</label>
-            <input name="confirm" value={userUpdate.confirm} type="text" onChange={(e) => handleChange(e, userUpdate, setUserUpdate)}/>
-          </>
-        ) : (
-          ""
-        )}
-        <button
+                  {/* של יצחק ק */}
+                  {/* <input name="password" type="text" onChange={(e) => currntpassword(e)} /> */}
+                  <label>{hebrewVariables.newPassword}</label>
+                  <input name="newPassword" value={userUpdate.newPassword} type="text" onChange={(e) => handleChange(e, userUpdate, setUserUpdate)} />
+                  <label>{hebrewVariables.confirmPassword}</label>
+                  <input name="confirm" value={userUpdate.confirm} type="text" onChange={(e) => handleChange(e, userUpdate, setUserUpdate)} />
+                  <button
+                    className='btn'
+                    onClick={() => { dispatch(updateStaffPassword({ ...user,currentPassword:userUpdate.currentPassword, newPassword: userUpdate.newPassword })) }}
+                  >
+
+
+                    {hebrewVariables.updatePassword}
+                  </button>
+                  <button className='btn' onClick={() => setChangePassword(false)}>
+                    ביטול
+                  </button>
+                </>
+              ) : (
+                ""
+              )}
+              {/* <button
         className='btn'
           onClick={() => {setChangePassword(changePassword ? false : true); dispatch(updateStaffPassword({...user , currentPassword:userUpdate.password, newPassword:userUpdate.newPassword }))}}
         >
-          {hebrewVariables.updatePassword}
-        </button>
-        <button className='btn' onClick={() => dispatch(updateUser(userUpdate,file))}>
-          {hebrewVariables.update}
-        </button>
+          {hebrewVariables.updatePassword} 
+        </button> */}
+              {changePassword === false ? (<button
+                className='btn'
+                onClick={() => { setChangePassword(true) }}
+              >
+                {hebrewVariables.updatePassword}
+              </button>) : ("")}
+              <button className='btn' onClick={() => dispatch(updateUser(userUpdate, file))}>
+                {hebrewVariables.update}
+              </button>
 
-        </div>
-       
-      </form>
-      <button style={{width:"100%"}} className='btn' onClick={() => setEditProfile(false)}>
-        {hebrewVariables.closeBtn}
-      </button>
-      </Box>
+            </div>
+
+          </form>
+          <button style={{ width: "100%" }} className='btn' onClick={() => setEditProfile(false)}>
+            {hebrewVariables.closeBtn}
+          </button>
+        </Box>
       </Modal>
     </div>
   );
--- client/src/Redux/actions/staffAction.js
+++ client/src/Redux/actions/staffAction.js
@@ -1,4 +1,4 @@
-import { GET_STAFF_LIST, ADD_STAFF, DELETE_STAFF, STAFF_ERRORS, UPDATE_STAFF,UPDATE_STAFF_PASSWORD } from "./types";
+import { GET_STAFF_LIST, ADD_STAFF, DELETE_STAFF, STAFF_ERRORS, UPDATE_STAFF, UPDATE_STAFF_PASSWORD } from "./types";
 import fetcher from "../../utils/fetcher";
 
 export const getStaff = () => async dispatch => {
@@ -11,18 +11,18 @@ export const getStaff = () => async dispatch => {
         .catch((err) => console.log(err));
 }
 
-export const addStuff = (staff,file) => async dispatch => {
-    const staffObj= new FormData()
+export const addStuff = (staff, file) => async dispatch => {
+    const staffObj = new FormData()
     staffObj.append('profileImg', file)
-    staffObj.append('registeredAs',staff.registeredAs)
-    staffObj.append('firstName',staff.firstName||"")
-    staffObj.append('lastName',staff.lastName||"")
-    staffObj.append('email',staff.email||"")
-    staffObj.append('password',staff.password||"")
-    staffObj.append('age',staff.age||"")
-    staffObj.append('jod',staff.jod||"")
-    staffObj.append('responsible',staff.responsible||"")
-    staffObj.append('phone',staff.phone||"")
+    staffObj.append('registeredAs', staff.registeredAs)
+    staffObj.append('firstName', staff.firstName || "")
+    staffObj.append('lastName', staff.lastName || "")
+    staffObj.append('email', staff.email || "")
+    staffObj.append('password', staff.password || "")
+    staffObj.append('age', staff.age || "")
+    staffObj.append('jod', staff.jod || "")
+    staffObj.append('responsible', staff.responsible || "")
+    staffObj.append('phone', staff.phone || "")
     try {
         await fetch(`http://localhost:8080/api/register`, {
             method: "POST",
@@ -72,7 +72,7 @@ export const deleteStaff = (staffId) => async dispatch => {
 }
 
 export const updateStaff = (updateStaff) => async dispatch => {
-    
+
     const { id } = { ...updateStaff };
     await fetcher(`http://localhost:8080/api/staff/update/${id}`, {
         method: 'PUT',
@@ -86,14 +86,28 @@ export const updateStaff = (updateStaff) => async dispatch => {
 }
 
 export const updateStaffPassword = (updateStaff) => async dispatch => {
-   
+
     // const { _id } = { ...updateStaff };
     // debugger
+    console.log(updateStaff.currentPassword)
     await fetcher(`http://localhost:8080/api/staff/changePassword`, {
         method: 'PUT',
-        body: JSON.stringify(updateStaff)
+        headers: {
+            "Accept": "apllication/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: updateStaff._id,
+            title: updateStaff.role,
+            currentPassword: updateStaff.currentPassword,
+            newPassword: updateStaff.newPassword,
+            firstName: updateStaff.firstName,
+            lastName: updateStaff.lastName,
+            email: updateStaff.email,
+            password: updateStaff.password
+        })
     })
-    .then(response =>console.log(response))
+        .then(response => console.log(response))
         .then(response => dispatch({
             type: UPDATE_STAFF_PASSWORD,
             payload: response
