54895b8d68300ed8e33f70be2c65a639a1c30ec8
SHUANESH <shuanesh1817@gmail.com>
2021-10-03 13:35:21 +0300

    server-deploy

--- server/server.js
+++ server/server.js
@@ -40,10 +40,13 @@ app.use("/api/homework", isToken, routeHomework);
 if (process.env.NODE_ENV === "production") {
 }
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "/client/build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res)=>{
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
 
 const server = app.listen(PORT, () => {
   console.log(
