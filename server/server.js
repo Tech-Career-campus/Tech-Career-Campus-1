efe6680a2759206312df407d5fef25c8fe2aec52
SHUANESH <shuanesh1817@gmail.com>
2021-10-03 13:16:09 +0300

    production-server

--- server/server.js
+++ server/server.js
@@ -39,10 +39,12 @@ app.use("/api/homework", isToken, routeHomework);
 
 if (process.env.NODE_ENV === "production") {
 }
+
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/client/build", "index.html"));
 });
+
 const server = app.listen(PORT, () => {
   console.log(
     `${chalk.green("tech_career")} ${chalk.yellow(
