f6a4a687184a2498e51a272dde45b8c66abd01cf
SHUANESH <shuanesh1817@gmail.com>
2021-10-03 05:17:50 +0300

    test herok

--- client/src/Components/Features/EditProfile/EditProfileConponent.jsx
+++ client/src/Components/Features/EditProfile/EditProfileConponent.jsx
@@ -5,7 +5,6 @@ import handleChange from "../../../utils/handleChange";
 import { hebrewVariables } from "../../../utils/hebrewVariables";
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
-import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import './editProfile.css'
 import {
   updateStaffPassword,
